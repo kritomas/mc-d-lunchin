33e5a9aede38d2b16c7dd69d4cde096bf59cfab6
Sharkpb8 <sharkpb8@gmail.com>
2025-02-11 16:11:22 +0100

    Update: same password check

--- mc-d-lunchin-backend/register.js
+++ mc-d-lunchin-backend/register.js
@@ -1,12 +1,15 @@
 import { DBConnection } from './DBC.js';
 import bcrypt from 'bcrypt';
 
-export async function RegisterUser(name, last_name, phone, email, username, password) {
+export async function RegisterUser(name, last_name, phone, email, username, password, password_check) {
     const pool = await DBConnection();
     const hashedPassword = await bcrypt.hash(password, 10);
     const connection = await pool.getConnection();
     
     try {
+        if(password !== password_check){
+            return { success: false, message: "Hesla se neschodují" };
+        }
         await connection.beginTransaction();
         const [user_rows] = await pool.execute('INSERT INTO user (name, last_name, phone) VALUES (?, ?, ?)', [name, last_name, phone]);
         let user_id = user_rows.insertId
