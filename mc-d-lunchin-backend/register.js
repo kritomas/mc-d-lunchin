55e814b4265b13e00464edf70f11c67ce743c132
Sharkpb8 <sharkpb8@gmail.com>
2025-02-11 14:50:13 +0100

    Added: register

---
+++ mc-d-lunchin-backend/register.js
@@ -0,0 +1,21 @@
+import { DBConnection } from './DBC.js';
+import bcrypt from 'bcrypt';
+
+export async function RegisterUser(name, last_name, phone, email, username, password) {
+    const pool = await DBConnection();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const connection = await pool.getConnection();
+    
+    try {
+        await connection.beginTransaction();
+        const [user_rows] = await pool.execute('INSERT INTO user (name, last_name, phone) VALUES (?, ?, ?)', [username, last_name, phone]);
+        let user_id = rows.insertId
+        const [credentials_rows] = await pool.execute('INSERT INTO credentials (email, username, password, user_id) VALUES (?, ?, ?, ?)', [email, username, hashedPassword, user_id]);
+
+        await connection.commit();
+        return { success: true, userId: credentials_rows.insertId };
+    } catch (error) {
+        await connection.rollback();
+        return { success: false, message: error.message };
+    }
+}
