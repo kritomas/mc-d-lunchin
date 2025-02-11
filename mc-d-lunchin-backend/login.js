feb8459e43e10141674a089cb0c33a12b251f47f
Sharkpb8 <sharkpb8@gmail.com>
2025-02-11 14:58:26 +0100

    Fix: correct login check

--- mc-d-lunchin-backend/login.js
+++ mc-d-lunchin-backend/login.js
@@ -1,24 +1,30 @@
 import { DBConnection } from './DBC.js';
 import bcrypt from 'bcrypt';
 
-export async function LoginUser(username, password) {
+export async function LoginUser(name_email, password) {
     const pool = await DBConnection();
     
     try {
-        const [rows] = await pool.execute('SELECT * FROM users WHERE username = ?', [username]);
+        const [username_rows] = await pool.execute('SELECT  password,user_id FROM credentials WHERE username = ?', [name_email]);
         
-        if (rows.length === 0) {
-            return { success: false, message: 'User not found' };
+        if (username_rows.length === 0) {
+            const [email_rows] = await pool.execute('SELECT password,user_id FROM credentials WHERE email = ?', [name_email]);
+            if (email_rows.length === 0) {
+                return { success: false, message: 'User not found' };
+            }else{
+                user = username_rows[0]
+            }
+        }else{
+            user = username_rows[0]
         }
 
-        const user = rows[0];
         const passwordMatch = await bcrypt.compare(password, user.password);
         
         if (!passwordMatch) {
             return { success: false, message: 'Invalid password' };
         }
 
-        return { success: true, userId: user.id };
+        return { success: true, userId: user.user_id };
     } catch (error) {
         return { success: false, message: error.message };
     }
