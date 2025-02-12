57e97df9fcb4d4f0a88c29e21f972e41088a521d
kritomas <Unknown>
2025-02-12 21:25:53 +0100

    Added endpoint for login.

--- mc-d-lunchin-backend/main.js
+++ mc-d-lunchin-backend/main.js
@@ -1,9 +1,32 @@
 import express from "express";
 import cors from "cors";
+import {LoginUser} from "./login.js";
 
 const PORT = 42069;
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+app.put("/api/user", async (req, res, next) => // Login endpoint
+{
+	try
+	{
+		const {username, password} = req.body;
+		const result = await LoginUser(username, password);
+		if (result.success)
+		{
+			res.status(200).send({id: result.userId});
+		}
+		else
+		{
+			res.status(500).send(result.message);
+		}
+	}
+	catch (e)
+	{
+		next(e);
+	}
+});
 
 app.use(express.static("dist"));
 
