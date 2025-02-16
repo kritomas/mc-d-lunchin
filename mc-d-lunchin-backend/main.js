31eb614d4d16e2f51a26e2f48d91ee824eeccbdc
kritomas <Unknown>
2025-02-16 13:19:15 +0100

    Added review endpoints.

--- mc-d-lunchin-backend/main.js
+++ mc-d-lunchin-backend/main.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import {LoginUser} from "./login.js";
 import {getAllFoodData} from "./Food_filter.js";
+import {createReview, updateReview} from "./review.js";
 
 const PORT = 42069;
 const app = express();
@@ -36,6 +37,47 @@ app.put("/api/food", async (req, res, next) =>
 		const {category_whitelist, category_blacklist, type, is_vegetarian} = req.body;
 		const result = await getAllFoodData(category_whitelist, category_blacklist, type, is_vegetarian);
 		res.status(200).send(result);
+	}
+	catch (e)
+	{
+		next(e);
+	}
+});
+
+app.post("/api/review", async (req, res, next) =>
+{
+	try
+	{
+		const {user_id, food_id, rating, comment, portion_size, temperature, appearance, extra_pay, cook_recommendation} = req.body;
+		const result = await createReview(user_id, food_id, rating, comment, portion_size, temperature, appearance, extra_pay, cook_recommendation);
+		if (result.success)
+		{
+			res.status(200).send(result.message); // TODO Return ID of review
+		}
+		else
+		{
+			res.status(500).send(result.message);
+		}
+	}
+	catch (e)
+	{
+		next(e);
+	}
+});
+app.patch("/api/review", async (req, res, next) =>
+{
+	try
+	{
+		const {id, rating, comment, portion_size, temperature, appearance, extra_pay, cook_recommendation} = req.body;
+		const result = await updateReview(id, rating, comment, portion_size, temperature, appearance, extra_pay, cook_recommendation);
+		if (result.success)
+		{
+			res.status(200).send(result.message);
+		}
+		else
+		{
+			res.status(500).send(result.message);
+		}
 	}
 	catch (e)
 	{
